196f38faa77dda1e6ee3dd40543c2d62c0468ce9
bralegz <bralegz@gmail.com>
2025-01-16 20:43:15 -0500

    adding tests for the first set of requirements

--- src/test/pass_checker/PasswordChecker.test.ts
+++ src/test/pass_checker/PasswordChecker.test.ts
@@ -8,7 +8,41 @@ describe('PasswordChecker test suite', () => {
     sut = new PasswordChecker();
   });
 
-  it('It should do nothing for the moment', () => {
-    sut.checkPassword();
+  it('It should return false if the password length is less than 8', () => {
+    const actual = sut.checkPassword('1234567');
+    expect(actual).toBe(false);
   });
+
+  it('It should return true if the password is at least 8', () => {
+    const actual = sut.checkPassword('12345678Aa');
+
+    expect(actual).toBe(true);
+  });
+
+
+  it('Password with no uppercase letter is invalid', () => {
+    const actual = sut.checkPassword('1234abcd');
+
+    expect(actual).toBe(false);
+  });
+
+  it('Password with uppercase letters is ok', () => {
+    const actual = sut.checkPassword('1234abcdA');
+
+    expect(actual).toBe(true);
+  })
+
+  it('Password with no lowercase letter is invalid', () => {
+    const actual = sut.checkPassword('1232ABCD');
+
+    expect(actual).toBe(false);
+  });
+
+  it('Password with lowercase letters is ok', () => {
+    const actual = sut.checkPassword('1232ABCDa');
+
+    expect(actual).toBe(true);
+  });
+
+
 });
