4e221f3a5a2664616f51a95d97179336eb44fc66
bralegz <bralegz@gmail.com>
2025-01-17 22:10:36 -0500

    fake double implementation

--- src/app/doubles/OtherUtils.ts
+++ src/app/doubles/OtherUtils.ts
@@ -6,7 +6,20 @@ export type stringInfo = {
   extraInfo: Object | undefined;
 };
 
+type LoggerServiceCallBack = (arg: string) => void;
+
 export function calculateComplexity(stringInfo: stringInfo) {
   //calculates the complexity of the objects multiplying the number of keys of extra info by the number of keys of string Info itself
   return Object.keys(stringInfo.extraInfo).length * stringInfo.length;
+}
+
+export function toUpperCaseWithCallback(arg: string, callBack: LoggerServiceCallBack) {
+  if (!arg) {
+    callBack('Invalid Argument!');
+    return;
+  }
+
+  callBack(`Called function with ${arg}`);
+
+  return arg.toUpperCase();
 }
--- src/test/doubles/OtherUtils.test.ts
+++ src/test/doubles/OtherUtils.test.ts
@@ -1,6 +1,19 @@
-import { calculateComplexity } from '../../app/doubles/OtherUtils';
+import { calculateComplexity, toUpperCaseWithCallback } from '../../app/doubles/OtherUtils';
 
 describe('OtherUtils test suite', () => {
+  it('ToUpperCase - calls callback for invalid argument', () => {
+    //The fake is the callback function that we are passing just to help us to perform the test. In reality this function might be doing something but in this case it is suficcient if the callback does nothing.
+    const actual = toUpperCaseWithCallback('', () => {});
+    expect(actual).toBeUndefined();
+  })
+
+
+  it('ToUpperCase - calls callback for invalid argument', () => {
+    const actual = toUpperCaseWithCallback('abc', () => {});
+    expect(actual).toBe('ABC');
+  });
+
+
   it('Calculates complexity', () => {
     //This is an example of stubs. They are just incomplete objects with just the information we need. We won't use them inside the assertions. They are just helper objects to help us to do our tests
     const someInfo = {
@@ -15,4 +28,6 @@ describe('OtherUtils test suite', () => {
 
     expect(actual).toBe(10);
   });
+
+
 });
